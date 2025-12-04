2cf89c0c286605db699a8186ee6de47b852146e5
owuordickson <owuordickson@gmail.com>
2025-12-04 17:24:31 -0500

    v1.0.6: imposed potential; capacitance and leak-resistivity?

--- src/rgtlib/__init__.py
+++ src/rgtlib/__init__.py
@@ -6,8 +6,8 @@ A software package for
 """
 
 # Project Details
-__version__ = "1.0.5"
-__install_version__ = "1.0.5"
+__version__ = "1.0.6"
+__install_version__ = "1.0.6"
 __title__ = "ResponseGT"
 __author__ = "William Stephenson and Dickson Owuor"
 __credits__ = "The Regents of the University of Michigan"
